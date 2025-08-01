117cb1f56e687f022d9f8446c83293e545c40e1a
atambek <atambekbrat@gmail.com>
2025-08-01 10:41:33 +0600

    update req.txt

--- core/core/urls.py
+++ core/core/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
 
     # spectacular schema + swagger + redoc
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
